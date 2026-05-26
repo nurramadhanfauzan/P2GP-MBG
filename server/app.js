ac0cdc6699f544d712e3f4fc38ec090f52ad6b2a
Talitha Najwa Afifah <najwapei23@gmail.com>
2026-05-26 23:59:21 +0700

    refactor: simplify socket server setup

--- server/app.js
+++ server/app.js
@@ -1,13 +1,37 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const http = require('http')
+const { Server } = require('socket.io')
 const router = require('./routers')
 const errorHandler = require('./middlewares/errorHandler')
+const socketHandler = require('./socket')
 const app = express()
 
 app.use(cors())
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(router)
 app.use(errorHandler)
-module.exports = app
+
+const server = http.createServer(app)
+
+const io =
+    new Server(server, {
+        cors: {
+            origin: '*'
+        }
+    })
+
+socketHandler(io)
+
+const PORT =
+    process.env.PORT || 3000
+
+server.listen(PORT, () => {
+
+    console.log(
+        `Server running on ${PORT}`
+    )
+})
