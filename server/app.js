4ad6b48e7bee85b031bceed8f956d3caa37e8590
Talitha Najwa Afifah <najwapei23@gmail.com>
2026-05-26 20:58:52 +0700

    feat: setup socket.io server infrastructure

---
+++ server/app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const router = require('./routers')
+const errorHandler = require('./middlewares/errorHandler')
+const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(router)
+app.use(errorHandler)
+module.exports = app
